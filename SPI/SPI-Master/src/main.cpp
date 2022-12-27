f5231559128fb0dbce8369f12a1b4f1f881eb9c6
Lynn Marek <georg.marek687@gmail.com>
2022-12-27 17:26:50 +0100

    SPI atart

---
+++ SPI/SPI-Master/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+// set pin 10 as the slave select for the digital pot:
+const int slaveSelectPin = 10;
+
+void setup() {
+  // set the slaveSelectPin as an output:
+  pinMode(slaveSelectPin, OUTPUT);
+  // initialize SPI:
+  SPI.begin();
+}
+
+void loop() {
+
+}
+
+void digitalPotWrite(int value) {
+  // take the SS pin low to select the chip:
+  digitalWrite(slaveSelectPin, LOW);
+  delay(100);
+  //  send in the address and value via SPI:
+  SPI.transfer(value);
+  delay(100);
+  // take the SS pin high to de-select the chip:
+  digitalWrite(slaveSelectPin, HIGH);
+}
