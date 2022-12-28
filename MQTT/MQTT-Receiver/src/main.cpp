78988f0a6d06527c8aef8cf4ea5ec254915bf879
Lynn Marek <georg.marek687@gmail.com>
2022-12-28 08:52:41 +0100

    added MQTT transmit and receive

--- MQTT/MQTT-Receiver/src/main.cpp
+++ MQTT/MQTT-Receiver/src/main.cpp
@@ -14,11 +14,9 @@ const char *mqtt_server = "iotmqtt.htl-klu.at";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsgMillis = 0;
 char msg[200];
-int count = 0;
 
-DynamicJsonDocument doc(200);
+StaticJsonDocument<200> doc;
 // ##########################################################################################################################################
 // METHODES
 void setup_wifi()
@@ -45,6 +43,42 @@ void setup_wifi()
   Serial.println(WiFi.localIP());
 }
 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  String inDataString = "";
+  for (int i = 0; i < length; i++){
+    inDataString += (char)payload[i];
+  }
+  Serial.println(inDataString);
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, inDataString);
+
+  // Test if parsing succeeds.
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  const char *sensor = doc["sensor"];
+  long time = doc["time"];
+  double latitude = doc["data"][0];
+  double longitude = doc["data"][1];
+
+  // Print values.
+  Serial.println(sensor);
+  Serial.println(time);
+  Serial.println(latitude, 6);
+  Serial.println(longitude, 6);
+  Serial.println();
+}
+
 void reconnect()
 {
   // Loop until we're reconnected
@@ -58,6 +92,8 @@ void reconnect()
     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password))
     {
       Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe(DATA_TOPIC);
     }
     else
     {
@@ -86,6 +122,7 @@ void setup()
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 // #########################################################################################################################################
 // LOOP
@@ -96,18 +133,4 @@ void loop()
     reconnect();
   }
   client.loop();
-
-  if (millis() - lastMsgMillis > 2000)
-  {
-    ++count;
-    doc["time"] = count;
-    
-    serializeJson(doc, msg);
-
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish(DATA_TOPIC, msg);
-
-    lastMsgMillis = millis();
-  }
 }
