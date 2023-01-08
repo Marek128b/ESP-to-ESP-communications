7b0390af475c3da41626c32dc11cccaac6f9d863
Lynn Marek <georg.marek687@gmail.com>
2023-01-08 15:30:35 +0100

    SPI-Master added

--- SPI/SPI-Master/src/main.cpp
+++ SPI/SPI-Master/src/main.cpp
@@ -4,18 +4,23 @@
 // set pin 10 as the slave select for the digital pot:
 const int slaveSelectPin = 10;
 
-void setup() {
+void setup()
+{
   // set the slaveSelectPin as an output:
   pinMode(slaveSelectPin, OUTPUT);
   // initialize SPI:
   SPI.begin();
 }
 
-void loop() {
-
+void loop()
+{
+  String data_tx_str = "hkjgahkgnkja";
+  uint8_t* data_tx = data_tx_str.toInt();
+  SPI.writeBytes(data_tx, sizeof(data_tx));
 }
 
-void digitalPotWrite(int value) {
+void digitalPotWrite(int value)
+{
   // take the SS pin low to select the chip:
   digitalWrite(slaveSelectPin, LOW);
   delay(100);
