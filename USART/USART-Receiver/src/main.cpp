c9e06ff0e49532c411b2b4b0c843c793160be707
Lynn Marek <georg.marek687@gmail.com>
2023-01-08 15:46:53 +0100

    transmitter and receiver working

--- USART/USART-Receiver/src/main.cpp
+++ USART/USART-Receiver/src/main.cpp
@@ -2,9 +2,6 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-String json =
-    "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-
 StaticJsonDocument<200> doc;
 
 void setup()
@@ -12,31 +9,7 @@ void setup()
   Serial.begin(115200);
   Serial2.begin(115200);
 
-  // Deserialize the JSON document
-  DeserializationError error = deserializeJson(doc, json);
-
-  // Test if parsing succeeds.
-  if (error)
-  {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
-
-  // Fetch values.
-  //
-  // Most of the time, you can rely on the implicit casts.
-  // In other case, you can do doc["time"].as<long>();
-  const char *sensor = doc["sensor"];
-  long time = doc["time"];
-  double latitude = doc["data"][0];
-  double longitude = doc["data"][1];
-
-  // Print values.
-  Serial.println(sensor);
-  Serial.println(time);
-  Serial.println(latitude, 6);
-  Serial.println(longitude, 6);
+  Serial.println("Receiver");
 }
 
 void loop()
@@ -57,7 +30,6 @@ void loop()
     }
 
     // Fetch values.
-    //
     // Most of the time, you can rely on the implicit casts.
     // In other case, you can do doc["time"].as<long>();
     const char *sensor = doc["sensor"];
--- USART/USART-Transmit/src/main.cpp
+++ USART/USART-Transmit/src/main.cpp
@@ -9,24 +9,27 @@ void setup()
   Serial.begin(115200);
   Serial2.begin(115200);
 
+  Serial.println("Transmitter");
+
   // Add values in the document
-  //
   doc["sensor"] = "gps";
   doc["time"] = 1351824120;
 
   // Add an array.
-  //
   JsonArray data = doc.createNestedArray("data");
   data.add(48.756080);
   data.add(2.302038);
+}
 
+void loop()
+{
+  static int counter = 0;
   // Generate the minified JSON and send it to the Serial port.
   String outData;
   serializeJson(doc, outData);
   Serial.println(outData);
   Serial2.println(outData);
 
-  serializeJson(doc, Serial);
   Serial.println();
 
   const char *sensor = doc["sensor"];
@@ -39,14 +42,13 @@ void setup()
   Serial.println(latitude);
   Serial.println(longitude);
 
-  doc["sensor"] = "test1234";
+  doc["time"] = counter;
+
+  counter++;
 
   serializeJson(doc, Serial);
   // The above line prints:
   // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
-}
 
-void loop()
-{
-  // not used in this example
+  delay(2000);
 }
